5249c19f1265d100c950932554fb16aa2358cfab
Sidnei Rodrigues <sidnei.c.rodrigues@hotmail.com>
2022-11-18 19:25:56 -0300

    add Tailwind CSS

--- src/AppStyles.tsx
+++ src/AppStyles.tsx
@@ -1,98 +1,108 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-// DIV PRINCIPAL
+// // DIV PRINCIPAL
 
-export const Container = styled.div`
-    padding-top: 40px;
-    width: auto;
-    height: 100vh;
-    background-color: #201b2c;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
+// type ContainerProps = {
+//     bgColor: string;
+// }
 
-// DIV HEADER
+// export const Container = styled.div<ContainerProps>`
+//     padding-top: 40px;
+//     width: auto;
+//     height: 100vh;
+//     background-color: ${ props => props.bgColor };
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+// `
 
-export const Header = styled.header`
-    width: 50vw;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-`
+// // DIV HEADER
 
-export const Title = styled.h1`
-    font-size: 2.5vw;
-    color: #40C4FF;
-`
+// export const Header = styled.header`
+//     width: 50vw;
+//     height: 100vh;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     flex-direction: column;
+// `
 
-export const ImgPrimary = styled.img`
-    width: 35vw;
-`
+// export const Title = styled.h1`
+//     font-size: 2.5vw;
+//     color: #40C4FF;
+// `
 
-// DIV ASIDE FORM
+// export const ImgPrimary = styled.img`
+//     width: 35vw;
+// `
 
-export const AsideForm = styled.aside`
-    width: 50vw;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
+// // DIV ASIDE FORM
 
-export const Form = styled.form`
-    width: 60%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    padding: 30px 35px;
-    background-color: #2f2841;
-    border-radius: 20px;
-    box-shadow: 0px 10px 40px #00000056;
-`
+// export const AsideForm = styled.aside`
+//     width: 50vw;
+//     height: 100vh;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+// `
 
-export const FormDiv = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: center;
-    margin: 10px 0px;
-`
+// export const Form = styled.form`
+//     width: 60%;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     flex-direction: column;
+//     padding: 30px 35px;
+//     background-color: #2f2841;
+//     border-radius: 20px;
+//     box-shadow: 0px 10px 40px #00000056;
+// `
 
-export const FormLabel = styled.label`
-    color: #f0ffffde;
-    margin-bottom: 10px;
-`
+// export const FormDiv = styled.div`
+//     width: 100%;
+//     display: flex;
+//     flex-direction: column;
+//     align-items: flex-start;
+//     justify-content: center;
+//     margin: 10px 0px;
+// `
 
-export const FormInput = styled.input`
-    width: 100%;
-    border: none;
-    border-radius: 10px;
-    padding: 15px;
-    background-color: #514869;
-    color: #f0ffffde;
-    font-size: 12pt;
-    box-shadow: 0px 10px 40px #00000056;
-    outline: none;
-    box-sizing: border-box;
-`
+// export const FormLabel = styled.label`
+//     color: #f0ffffde;
+//     margin-bottom: 10px;
+// `
 
-export const FormBtn = styled.button`
-    width: 100%;
-    padding: 16px 0px;
-    margin: 25px;
-    border: none;
-    border-radius: 8px;
-    outline: none;
-    text-transform: uppercase;
-    font-weight: 800;
-    letter-spacing: 3px;
-    color: #2b134b;
-    background-color: #FFC107;  
-    cursor: pointer;
-    box-shadow: 0px 10px 40px -12px #00ff8052; 
-`
+// export const FormInput = styled.input`
+//     width: 100%;
+//     border: none;
+//     border-radius: 10px;
+//     padding: 15px;
+//     background-color: #514869;
+//     color: #f0ffffde;
+//     font-size: 12pt;
+//     box-shadow: 0px 10px 40px #00000056;
+//     outline: none;
+//     box-sizing: border-box;
+// `
+
+// type BotaoProps = {
+//     bg: string;
+//     small?: boolean;
+// }
+
+
+// export const FormBtn = styled.button<BotaoProps>`
+//     width: ${ props => props.small ? '50%' : '100%' };
+//     background-color: ${ props => props.bg};  
+//     padding: 16px 0px;
+//     margin: 25px;
+//     border: none;
+//     border-radius: 8px;
+//     outline: none;
+//     text-transform: uppercase;
+//     font-weight: 800;
+//     letter-spacing: 3px;
+//     color: #2b134b;    
+//     cursor: pointer;
+//     box-shadow: 0px 10px 40px -12px #00ff8052; 
+// `
